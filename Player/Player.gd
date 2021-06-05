d5a9ddd3543c6ac98a9fba96e8082c4c8534a49b
Patryk Czerech <the4ech@gmail.com>
2021-06-05 11:19:43 +0200

    Add tmp floor and improve keybinding for jumping

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
-const GRAVITY = 15
+const GRAVITY = 25
 const JUMP_SPEED = -600
 var velocity = Vector2.ZERO
 
 func _process(delta):
 	velocity.y += GRAVITY 
 	
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("jump") and is_on_wall():
 		velocity.y = JUMP_SPEED
 	
 	move_and_slide(velocity)
