3276f2a1082757b29307c37547fc31d83bda5129
Patryk Czerech <the4ech@gmail.com>
2021-06-05 10:57:12 +0200

    Add jumping for Player

---
+++ Player/Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+const GRAVITY = 15
+const JUMP_SPEED = -500
+var velocity = Vector2.ZERO
+
+func _process(delta):
+	velocity.y += GRAVITY 
+	
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = JUMP_SPEED
+	
+	move_and_slide(velocity)
+	
